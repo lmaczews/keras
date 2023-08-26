4b6b847a96f8ad2d935a4cd6828542378c183b98
Lukasz Maczewski <lukaszmaczewski@lukaszs-macbook-pro.home>
2023-08-26 19:56:21 +0200

    class to support text sequence processing

---
+++ kst/data_sets/TextSequence.py
@@ -0,0 +1,60 @@
+import re
+from collections import defaultdict
+
+from gensim.parsing.preprocessing import preprocess_string
+
+
+class TextSequence():
+
+    def __init__(self, begin_of_sequence='<BOS>', end_of_sequence='<EOS>'):
+        self.txt_sequence = []
+        self.token_count = defaultdict(int)
+        self.begin_of_sequence = begin_of_sequence
+        self.end_of_sequence = end_of_sequence
+
+    def s2s_text_preprocessing(self, text):
+        '''Clean text by removing unnecessary characters and altering the format of words.'''
+
+        text = text.lower()
+
+        text = re.sub(r"i'm", "i am", text)
+        text = re.sub(r"he's", "he is", text)
+        text = re.sub(r"she's", "she is", text)
+        text = re.sub(r"it's", "it is", text)
+        text = re.sub(r"that's", "that is", text)
+        text = re.sub(r"what's", "that is", text)
+        text = re.sub(r"where's", "where is", text)
+        text = re.sub(r"how's", "how is", text)
+        text = re.sub(r"\'ll", " will", text)
+        text = re.sub(r"\'ve", " have", text)
+        text = re.sub(r"\'d", " would", text)
+        text = re.sub(r"\'re", " are", text)
+        text = re.sub(r"won't", "will not", text)
+        text = re.sub(r"can't", "cannot", text)
+        text = re.sub(r"n't", " not", text)
+        text = re.sub(r"n'", "ng", text)
+        text = re.sub(r"'bout", "about", text)
+        text = re.sub(r"'til", "until", text)
+        text = re.sub(r"[-()\"#/@;:<>{}`+=~|.!?,]", "", text)
+
+        return text
+
+    def add_element(self, text, gensim_custom_tokenizer=None, initial_preprocessing=True):
+        if initial_preprocessing:
+            text = self.s2s_text_preprocessing(text)
+
+        if gensim_custom_tokenizer is not None:
+            tokens = preprocess_string(text, gensim_custom_tokenizer)
+        else:
+            tokens = text.split(' ')
+
+        for token in tokens:
+            self.token_count[token] += 1
+
+        self.txt_sequence += [tokens]
+
+    def pair_sequence(self):
+        i = 0
+        while len(self.txt_sequence) > i + 1:
+            yield self.txt_sequence[i], [self.begin_of_sequence] + self.txt_sequence[i + 1] + [self.end_of_sequence]
+            i += 1
